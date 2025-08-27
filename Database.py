c005e1f9ce1d94f0ef60d1cd1afc4b6cf83bea58
Sebastian Lema <sebasjhonlemamontoya12@gmail.com>
2025-08-27 11:43:26 -0500

    Reorganizar la configuración de la base de datos para usar una ruta relativa

--- Database.py
+++ Database.py
@@ -5,7 +5,10 @@ import bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'tu-clave-secreta-aqui'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///simulacros.db'
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(BASE_DIR, 'instance', 'simulacros.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
