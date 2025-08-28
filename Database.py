f581f56f2f9e52bcc14f04b47e1d9e018011f1b2
Sebastian Lema <sebasjhonlemamontoya12@gmail.com>
2025-08-28 13:28:39 -0500

    Actualizar la URI de la base de datos a MySQL

--- Database.py
+++ Database.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'tu-clave-secreta-aqui'
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-db_path = os.path.join(BASE_DIR, 'instance', 'simulacros.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1/simulacro.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
